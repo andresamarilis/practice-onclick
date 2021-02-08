baca68850aabe34eb2551d8169c81fc4a32631a0
andresamarilis <andres.amarilis@student.uts.edu.au>
2021-02-08 15:25:36 +1100

    p1

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+//code to change the paragraph in card 1
+const buttonChangeText = document.querySelector('.button1'); 
+
+buttonChangeText.addEventListener('click', () => {
+    document.getElementById("p1").innerHTML = "New text!"; 
+})
+
+//code to change the title in card 2
+const buttonChangeTitle = document.querySelector('.button2'); 
+
+buttonChangeTitle.addEventListener('click', () => {
+    document.getElementById("h2").innerHTML = "New Title!"; 
+})
+
+//code to remove third card
+
+
+
+
+
